f2cac872ba5bad274b85f3aebcebccb7d53dad32
JayKumar102030405060707090 <166695990+JayKumar102030405060707090@users.noreply.github.com>
2025-02-22 17:53:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -11,17 +11,17 @@ id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
 SESSION = environ.get('SESSION', 'TechVJBot')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
-BOT_TOKEN = environ.get('BOT_TOKEN', "")
+API_ID = int(environ.get('API_ID', '28609964'))
+API_HASH = environ.get('API_HASH', '3236bdf1e11a0fb8091495e083857621')
+BOT_TOKEN = environ.get('BOT_TOKEN', "7642271614:AAG4QWpg1sXQhZVSOu0OvmgkfPwygvkb8BA")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split()
+PICS = (environ.get('PICS', 'https://graph.org/file/9a24da4c42b10e53364a3-efa5e8ab93c0cea435.jpg')).split()
 
 
 # Admins & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()] # For Multiple Id Use One Space Between Each.
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7168729089').split()] # For Multiple Id Use One Space Between Each.
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]  # For Multiple Id Use One Space Between Each.
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 
